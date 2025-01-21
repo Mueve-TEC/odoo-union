b08b1f6f507dec7ed05dae4c879670b30f00c772
Ezequiel Ludueña <ezeluduena123@gmail.com>
2025-01-21 20:55:24 -0300

    fix: breadcrumb de configuración

--- affiliation/models/affiliation_configuration.py
+++ affiliation/models/affiliation_configuration.py
@@ -8,20 +8,24 @@ class AffiliationConfiguration(models.Model):
     _name = 'affiliation.affiliation_configuration'
     _description = 'Configuration of Affiliaton module'
 
-    current_affiliation_number = fields.Integer(string='Current affiliation number')
+    # TODO: Cargar traducción para name
+    # Este campo se agregó para que el breadcrumb no figure como "affiliation.affiliation_configuration,1"
+    name = fields.Char(string='Name', default='Configuración')
+    current_affiliation_number = fields.Integer(
+        string='Current affiliation number')
     next_affiliation_number = fields.Integer(string='Next affiliation number')
     set_affiliation_date = fields.Selection(
         selection=[
-            ('on_affiliate','On affiliate'),
-            ('on_confirm','On confirm affiliation'),
+            ('on_affiliate', 'On affiliate'),
+            ('on_confirm', 'On confirm affiliation'),
         ],
         string='Set init of affiliation',
         default='on_confirm'
     )
     set_disaffiliation_date = fields.Selection(
         selection=[
-            ('on_disaffiliate','On disaffiliate'),
-            ('on_confirm','On confirm disaffiliation'),
+            ('on_disaffiliate', 'On disaffiliate'),
+            ('on_confirm', 'On confirm disaffiliation'),
         ],
         string='Set end of affiliation',
         default='on_confirm'
@@ -29,7 +33,8 @@ class AffiliationConfiguration(models.Model):
 
     def write(self, vals):
         if 'next_affiliation_number' in vals:
-            _seq = self.env['ir.sequence'].search([('code','=','adiuc_affiliation_number_seq')])
-            _seq.number_next_actual = vals['next_affiliation_number'] 
+            _seq = self.env['ir.sequence'].search(
+                [('code', '=', 'adiuc_affiliation_number_seq')])
+            _seq.number_next_actual = vals['next_affiliation_number']
         res = super(AffiliationConfiguration, self).write(vals)
         return res
