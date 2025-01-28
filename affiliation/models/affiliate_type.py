3d1c11ed6cce62e594bf858473a6cd884dde46f6
Ezequiel Ludueña <ezeluduena123@gmail.com>
2025-01-28 14:10:59 -0300

    fix: import en affiliate_type

--- affiliation/models/affiliate_type.py
+++ affiliation/models/affiliate_type.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from odoo.exceptions import ValidationError
 
 class AffiliateType(models.Model):
     _name = 'affiliation.affiliate_type'
