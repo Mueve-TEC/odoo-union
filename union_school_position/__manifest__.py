1c70a62b310e9bc5fd399ac454a1896e444c9016
Ezequiel Ludueña <ezeluduena123@gmail.com>
2025-02-06 12:47:52 -0300

    refact: cambio de nombre en cargos

--- union_school_position/__manifest__.py
+++ union_school_position/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','affiliation'],
+    'depends': ['base','union_affiliation'],
 
     # always loaded
     'data': [
