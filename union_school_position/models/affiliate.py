e97f2811db46518d23dfe543a7c9e1790ae68f45
Ezequiel Ludueña <ezeluduena123@gmail.com>
2025-11-21 15:53:08 -0300

    feat: agrupar afiliados por cargos

--- union_school_position/models/affiliate.py
+++ union_school_position/models/affiliate.py
@@ -8,3 +8,15 @@ class Affiliate(models.Model):
         inverse_name='affiliate_id',
         string='Positions'
     )
+    
+    position_type_ids = fields.Many2many(
+        comodel_name='school_position.type',
+        string='Position Types',
+        compute='_compute_position_types',
+        store=True
+    )
+    
+    @api.depends('position_ids', 'position_ids.type_id')
+    def _compute_position_types(self):
+        for affiliate in self:
+            affiliate.position_type_ids = affiliate.position_ids.mapped('type_id')
