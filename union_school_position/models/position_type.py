f1a51b41248efe2e40cfca92a3f021ac12583fcb
Ezequiel Ludueña <ezeluduena123@gmail.com>
2026-05-18 19:28:07 -0300

    fix: visualización del nombre del tipo de cargo

--- union_school_position/models/position_type.py
+++ union_school_position/models/position_type.py
@@ -6,18 +6,24 @@ from odoo import models, fields, api
 class PositionType(models.Model):
     _name = 'school_position.type'
     _description = 'Type of school position'
-    _rec_name = 'code'
+    _rec_name = 'name'
 
     code = fields.Char(string="Code", required=True)
     name = fields.Char(string="Description", required=True)
     in_hours = fields.Boolean(string="Is in hours", required=True)
     dedication = fields.Char(string="Dedication", required=True)
 
+    def name_get(self):
+        result = []
+        for record in self:
+            name = f"[{record.code}] {record.name}" if record.code else record.name
+            result.append((record.id, name))
+        return result
+
     @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
+    def _name_search(self, name, args=None, operator='ilike', limit=100):
         args = args or []
-        domain = ['|',('name', operator, name),('code', operator, name)]
+        domain = ['|', ('name', operator, name), ('code', operator, name)]
         if 'import_file' in self.env.context:
             domain = [('code', operator, name)]
-        recs = self.search(domain + args, limit=limit)
-        return recs.name_get()
+        return self._search(domain + args, limit=limit)
