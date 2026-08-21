48e9f07ee60dab0fa1eb2b7be5f322d9b380ae71
Ezequiel Ludueña <ezeluduena123@gmail.com>
2026-08-21 13:44:00 -0300

    [IMP] apply ruff formatting to remaining files

--- union_affiliation/models/__init__.py
+++ union_affiliation/models/__init__.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from . import res_partner, affiliate_type, affiliate, affiliate_child, affiliation_period
+from . import (
+    res_partner,
+    affiliate_type,
+    affiliate,
+    affiliate_child,
+    affiliation_period,
+)
 from . import affiliation_configuration, workplace
