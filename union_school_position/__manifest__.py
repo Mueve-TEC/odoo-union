217810b949d409d8b92f39db39269a6ac4a71700
Ezequiel Ludueña <ezeluduena123@gmail.com>
2026-08-21 13:42:42 -0300

    [ADD] union_benefit_request, union_school_position: affiliation configuration for import-create-affiliate

--- union_school_position/__manifest__.py
+++ union_school_position/__manifest__.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Sindicato - Cargos",
-    'summary': """
+    "name": "Sindicato - Cargos",
+    "summary": """
         Módulo para gestionar los cargos.""",
-    'author': "Mueve",
+    "author": "Mueve",
     "website": "https://mueve.org.ar/",
-    'category': 'Union',
+    "category": "Union",
     "license": "GPL-3",
-    'version': '19.0.0.0.0',
-    'depends': ['base', 'union_affiliation'],
-    'data': [
-        'security/school_position_security.xml',
-        'security/ir.model.access.csv',
-        'views/position_views.xml',
-        'views/position_type_views.xml',
-        'views/position_character_views.xml',
-        'views/affiliate_views.xml',
-        'views/menu.xml',
+    "version": "19.0.1.0.0",
+    "depends": ["base", "union_affiliation"],
+    "data": [
+        "security/school_position_security.xml",
+        "security/ir.model.access.csv",
+        "views/position_views.xml",
+        "views/position_type_views.xml",
+        "views/position_character_views.xml",
+        "views/affiliation_configuration_view.xml",
+        "views/affiliate_views.xml",
+        "views/menu.xml",
     ],
     "installable": True,
 }
--- union_school_position/models/__init__.py
+++ union_school_position/models/__init__.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from . import affiliate
-from . import position, position_type, position_character, position_tag
+from . import (
+    affiliate,
+    affiliation_configuration,
+    position,
+    position_character,
+    position_registration_date,
+    position_tag,
+    position_type,
+)
